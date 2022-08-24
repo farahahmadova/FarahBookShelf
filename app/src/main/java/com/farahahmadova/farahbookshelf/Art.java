48b0fb3b1979262e986d678d61c4ce180ab3aa7d
Farah <ahmadovafarah083@gmail.com>
2022-08-24 12:54:55 +0400

    Permission Rationale&View Binding added

---
+++ app/src/main/java/com/farahahmadova/farahbookshelf/Art.java
@@ -0,0 +1,11 @@
+package com.farahahmadova.farahbookshelf;
+
+public class Art {
+    String name;
+    int id;
+
+    public Art(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+}
